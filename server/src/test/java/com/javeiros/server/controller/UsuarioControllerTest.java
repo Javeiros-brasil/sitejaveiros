379d1588b64cdb1b18e1a6b0ad7f2c2c7c30dfff
Erasmo Bezerra <erasmo.ads.tech@gmail.com>
2023-06-01 07:47:32 -0300

    jav-12: update testes unitarios do cadastrarUsuario controller

--- server/src/test/java/com/javeiros/server/controller/UsuarioControllerTest.java
+++ server/src/test/java/com/javeiros/server/controller/UsuarioControllerTest.java
@@ -2,24 +2,25 @@ package com.javeiros.server.controller;
 
 import com.javeiros.server.dto.UsuarioDTO;
 import com.javeiros.server.enums.PerfilCandidato;
+import com.javeiros.server.exception.EntidadeJaExisteException;
+import com.javeiros.server.exception.UsuarioNaoSalvoException;
 import com.javeiros.server.model.AreaDeAtuacao;
+import com.javeiros.server.model.Usuario;
 import com.javeiros.server.service.UsuarioService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 import java.util.Arrays;
-import java.util.Objects;
-
 
 
 @ExtendWith(MockitoExtension.class)
@@ -30,6 +31,8 @@ public class UsuarioControllerTest {
     private UsuarioService usuarioService;
     UsuarioDTO usuarioDTO;
 
+    Usuario novoUsuario;
+
 
     @BeforeEach
     void setup(){
@@ -38,37 +41,49 @@ public class UsuarioControllerTest {
                                     "erasmobezerra", PerfilCandidato.JUNIOR,
                                     Arrays.asList( new AreaDeAtuacao(1L,"BACKEND"),
                                                    new AreaDeAtuacao(2L, "FRONTEND")));
+
+        novoUsuario = new Usuario(1L, "Erasmo", "Bezerra", "27997512017",
+                                "Erasmo Bezerra#9245", "erasmo.ads.tech@gmail.com", "sd34r3ferf34f",
+                                "erasmobezerra", PerfilCandidato.JUNIOR,
+                                Arrays.asList( new AreaDeAtuacao(1L,"BACKEND"),
+                                        new AreaDeAtuacao(2L, "FRONTEND")));
     }
 
     @Test
-    void deveSalvarUsuario_CadastrarUsuario() {
-        /* Simulamos o método cadastrarUsuario() do serviço UsuarioService para que ele não faça nada (usando Mockito.doNothing()).
-        Isso é feito para isolar o teste e focar na verificação do controlador em si. */
-        Mockito.doNothing().when(usuarioService).cadastrarUsuario(Mockito.any(UsuarioDTO.class));
+    void cadastrarUsuario_Sucesso() {
+        when(usuarioService.cadastrarUsuario(usuarioDTO)).thenReturn(novoUsuario);
 
-        //  Guardarmos a resposta do método cadastrarUsuario() do controlador usuarioController, passando o objeto usuarioDTO como parâmetro.
-        ResponseEntity<String> response = usuarioController.cadastrarUsuario(usuarioDTO);
+        ResponseEntity<?> response = usuarioController.cadastrarUsuario(usuarioDTO);
 
-        // Verificando se status HTTP retornado do controlador é igual a CREATED. Se for diferente, o teste falhará.
         assertEquals(HttpStatus.CREATED, response.getStatusCode());
-
-        // Verificamos se a mensagem de resposta contém a mensagem  esperada. Se não contiver, o teste falhará.
-        assertEquals("Usuário cadastrado com sucesso", response.getBody());
+        assertEquals(novoUsuario, response.getBody());
+        verify(usuarioService, times(1)).cadastrarUsuario(usuarioDTO);
     }
 
     @Test
-    void deveRetornarBadRequest_CadastrarUsuario() {
-        // Simulamos uma exceção lançada pelo UsuarioServiço
-        Mockito.doThrow(RuntimeException.class).when(usuarioService).cadastrarUsuario(Mockito.any(UsuarioDTO.class));
+    void cadastrarUsuario_Conflito() {
+        String mensagemErro = "Este email já foi cadastrado por outro usuário.";
+        EntidadeJaExisteException excecao = new EntidadeJaExisteException(mensagemErro);
+        when(usuarioService.cadastrarUsuario(usuarioDTO)).thenThrow(excecao);
 
-        // Guardarmos a resposta do método cadastrarUsuario() do controlador usuarioController, passando o objeto usuarioDTO como parâmetro.
-        ResponseEntity<String> response = usuarioController.cadastrarUsuario(usuarioDTO);
+        ResponseEntity<?> response = usuarioController.cadastrarUsuario(usuarioDTO);
 
-        // Verificamos se o estado HTTP retornado na resposta é igual a BAD_REQUEST. Se for diferente, o teste falhará.
+        assertEquals(HttpStatus.CONFLICT, response.getStatusCode());
+        assertEquals(mensagemErro, response.getBody());
+        verify(usuarioService, times(1)).cadastrarUsuario(usuarioDTO);
+    }
+
+    @Test
+    void cadastrarUsuario_Erro() {
+        String mensagemErro = "Erro ao salvar o usuário.";
+        UsuarioNaoSalvoException excecao = new UsuarioNaoSalvoException(mensagemErro);
+        when(usuarioService.cadastrarUsuario(usuarioDTO)).thenThrow(excecao);
+
+        ResponseEntity<?> response = usuarioController.cadastrarUsuario(usuarioDTO);
+
         assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-
-        // Verificamos se a mensagem de resposta contém a mensagem de erro esperada. Se não contiver, o teste falhará.
-        assertEquals("Ocorreu um erro ao salvar o novo usuário, favor tente novamente", response.getBody());
+        assertEquals(mensagemErro, response.getBody());
+        verify(usuarioService, times(1)).cadastrarUsuario(usuarioDTO);
     }
 
 }
