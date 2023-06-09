ca6d3607961a3f3631f5c786fd5403182d022074
erasmo bezerra <erasmo.ads.tech@gmail.com>
2023-06-09 12:31:49 -0300

    jav-12: implementando documentação com Swagger OpenAPI

--- server/src/main/java/com/javeiros/server/ServerApplication.java
+++ server/src/main/java/com/javeiros/server/ServerApplication.java
@@ -1,8 +1,12 @@
 package com.javeiros.server;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@OpenAPIDefinition(info = @Info(title = "api-Javeiros", version = "1", description = "API do site Javeiros"))
 @SpringBootApplication
 public class ServerApplication {
 
--- server/src/main/java/com/javeiros/server/controller/UsuarioController.java
+++ server/src/main/java/com/javeiros/server/controller/UsuarioController.java
@@ -8,6 +8,10 @@ import com.javeiros.server.model.Usuario;
 import com.javeiros.server.repository.UsuarioRepository;
 import com.javeiros.server.service.UsuarioService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,8 +20,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "usuarios")
 @RestController
-@RequestMapping("/usuarios")
+@RequestMapping(value = "/usuarios", produces = {"application/json"})
 public class UsuarioController {
 
     @Autowired
@@ -26,7 +31,14 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
+
     @PostMapping("/cadastro")
+    @Operation(summary = "Realiza o cadastro de usuários", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usuário cadastrado com sucesso"),
+            @ApiResponse(responseCode = "409", description = "Este email já foi cadastrado por outro usuário."),
+            @ApiResponse(responseCode = "400", description = "Erro ao salvar o usuário.")
+    })
     public ResponseEntity<?> cadastrarUsuario(@RequestBody UsuarioDTO usuarioDTO) {
         try {
             usuarioService.cadastrarUsuario(usuarioDTO);
@@ -37,9 +49,13 @@ public class UsuarioController {
             return new ResponseEntity<>(p.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
-
-
     @PutMapping("/{id}")
+    @Operation(summary = "Atualiza os dados de um Usuário.", method = "PUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Dados do Usuário atualizado com sucesso"),
+            @ApiResponse(responseCode = "401", description = "Usuário não autentificado."),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado.")
+    })
     public ResponseEntity<Usuario> atualizarUsuario(@PathVariable Long id, @RequestBody Usuario usuarioAtualizado) {
 
         Usuario usuario = usuarioService.atualizarUsuario(id, usuarioAtualizado);
@@ -54,6 +70,11 @@ public class UsuarioController {
 
 
     @GetMapping("/filtro")
+    @Operation(summary = "Busca os dados dos usuários por nome e/OU por area de atuação.", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de Usuários por nome e/ou área de atuação"),
+            @ApiResponse(responseCode = "401", description = "Usuário não autentificado.")
+    })
     public ResponseEntity filtroUsuario(
             //@RequestParam faz com que os parâmetros do método se tornem parâmetros que podem ser passados na requisição.
             //Quando utilizado com "required = false", indica que o parâmetro não é obrigatório.
